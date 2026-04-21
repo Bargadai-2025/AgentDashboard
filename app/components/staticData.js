befdf683dc0a33ba2263247df171f24dc519a9ed
Yash Jadhav <yj0034046@gmail.com>
2026-04-21 23:50:46 +0530

    fix office lat long in static data

--- app/components/staticData.js
+++ app/components/staticData.js
@@ -2,10 +2,8 @@ export const agents = [
   {
     id: 1,
     name: "Elon Musk",
-    image:
-      "/dp_1774271193834.jpeg"
+    image: "/dp_1774271193834.jpeg",
     // "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Elon_Musk_-_54820081119_%28cropped%29.jpg/250px-Elon_Musk_-_54820081119_%28cropped%29.jpg"
-    ,
     location: {
       lat: 19.076,
       lng: 72.8777,
@@ -28,7 +26,7 @@ export const agents = [
         loan: "LN121",
         location: {
           lat: 19.118759832265702,
-          lng: 73.23417547111625
+          lng: 73.23417547111625,
         },
         faceConfidence: 98,
         liveness: 96,
@@ -49,10 +47,8 @@ export const agents = [
   {
     id: 2,
     name: "Random Person",
-    image:
-      "/my_pic_1774271182118.jpeg"
+    image: "/my_pic_1774271182118.jpeg",
     // "https://randomuser.me/api/portraits/women/44.jpg"
-    ,
     location: {
       lat: 19.03156696284031,
       lng: 72.95032576916336,
@@ -74,10 +70,8 @@ export const agents = [
   {
     id: 3,
     name: "Alan Turing",
-    image:
-      "/MK_1_1774270112152.jpeg"
+    image: "/MK_1_1774270112152.jpeg",
     //  "https://cdn.britannica.com/81/191581-050-8C0A8CD3/Alan-Turing.jpg"
-    ,
     location: {
       lat: 19.050570392422397,
       lng: 72.8213746380985,
@@ -99,10 +93,8 @@ export const agents = [
   {
     id: 4,
     name: "Napolean",
-    image:
-      "/PIC_1774270519498.jpg"
+    image: "/PIC_1774270519498.jpg",
     // "https://www.napoleon.org/wp-content/uploads/2020/05/napoleon_bas_uterwijk_1200_wider.jpg"
-    ,
     location: {
       lat: 19.208706198794577,
       lng: 72.97165716552347,
@@ -136,7 +128,9 @@ export const agents = [
 
 // Static office location — shared by all agents
 export const OFFICE = {
-  lat: 19.1133869510231,
-  lng: 72.91810580467191,
+  // lat: 19.1133869510231,
+  lat: 19.221205362778235,
+  lng: 73.09295477236344,
+  // lng: 72.91810580467191,
   name: "Bargad HQ",
 };
