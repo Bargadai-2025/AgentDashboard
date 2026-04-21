ee9d53d5970c8c2f60c7209bb0262d25a836c310
Yash Jadhav <yj0034046@gmail.com>
2026-04-21 12:31:04 +0530

    update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,16 @@ const nextConfig = {
   // Prevent Next.js from bundling server-only Node packages
   serverExternalPackages: ["cloudinary", "mongoose"],
   async rewrites() {
+    // This looks at your .env file first. If it's not present, it falls back to Railway
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "https://agent-backend-dashboard-production.up.railway.app";
     return [
       {
         source: '/api/:path*',
-        destination: 'https://agent-backend-dashboard-production.up.railway.app/api/:path*',
-        // destination: 'http://localhost:8000/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
+
 };
 
 export default nextConfig;
